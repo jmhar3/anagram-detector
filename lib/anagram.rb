de04eb97c049b280b027848d28b8c7a124fcbcbc
jmhar <jmhar@protonmail.com>
2021-09-07 15:04:46 +1000

    anagram

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize word
+        @word = word
+    end
+
+    def match array
+        array.select {|anagram| @word.split("").sort == anagram.split("").sort}
+    end
+end
